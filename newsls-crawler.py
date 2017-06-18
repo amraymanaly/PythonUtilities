107fa4b5d3ecfc02d3ec2b212a41c3edf9523677
amraymanaly <miroamr78@gmail.com>
2017-06-18 21:03:32 +0200

    Update newsls-crawler.py

--- newsls-crawler.py
+++ newsls-crawler.py
@@ -1,12 +1,18 @@
 #!/usr/bin/python2
-import bs4, mechanize, sys, os
+
+# TODO:
+#   1. Add Excel Spreadsheet support
+#   2. Check for invalid bench numbers
+
+import bs4, mechanize, argparse, string
 
 br = None
-options = {}
+options = None
 results = []
 
 class Result:
     def __init__(self, grade, benchno):
+        print('Collecting %d ..' % benchno)
         # POST stuff ..
         br.select_form(nr=1)
         br['grade'] = [grade]
@@ -14,11 +20,11 @@ class Result:
         br.submit()
         # Extracting marks ..
         bs = bs4.BeautifulSoup(br.response().read(), 'lxml')
-        info = bs.findAll('table')[4].findAll('tr')
+        info = bs.find(attrs={'class': 'table_cc'}).findAll('tr')
         self.name = str(info[0].contents[3].text).strip()
         self.marks = {}
         for row in info[1:]:
-            if len(row.contents) < 5: continue
+            if len(row.contents) != 5: continue
             line = str(row.contents[1].text).strip()
             subject = line[:-5].rstrip()
             try:
@@ -31,20 +37,43 @@ class Result:
         # Finalizing ..
         br.back()
 
-def help(e):
-    print('%s [grade: J1 -> S2] [benchnos] [output file]' % sys.argv[0])
-    sys.exit(e)
+def write_table(sort):
+    keys = sort.keys()
+    if options.fileformat == 'text':
+        with options.outfile as f:
+            for key in keys:
+                f.write('%s:\n' % key)
+                for i, o in enumerate(sort[key]):
+                    f.write('\t%02d. %-40s\t--->\t%.2f\n' % (i+1, o.name, o.marks[key]))
+                    if i == options.tops-1: break
+    elif options.fileformat == 'html':
+        with options.outfile as f:
+            f.write('<link rel="stylesheet" href="tablestyle.css" /><table><tr>')
+            for key in keys: f.write('<th>%s</th>' % key)
+            f.write('</tr>')
+            for y in range(0, options.tops):
+                f.write('<tr>')
+                for x in range(0, len(keys)):
+                    try:
+                        o = sort[keys[x]][y]
+                    except IndexError:
+                        continue
+                    f.write('<td>%02d. %s\t<span class="mark">%.2f</span></td>' % (y+1, string.join(o.name.split()[:3]), o.marks[keys[x]]))
+                f.write('</tr>')
+            f.write('</table>')
+    elif options.fileformat == 'excel':
+        raise NotImplementedError()
 
 def parse_args():
-    try:
-        options['grade'] = {'J1': '0', 'J2': '2', 'J3': '3', 'J4': '4', 'J5': '5', 'J6': '6',
-                            'M1': '7', 'M2': '8', 'M3': '9', 'S1': '10', 'S2': '11'}.get(sys.argv[1], None)
-        if options['grade'] is None: raise Exception()
-        options['out'] = sys.argv[-1]
-        open(options['out'], 'w').close()
-        options['benchs'] = [int(x) for x in list(set(sys.argv[2:-1]))]
-    except:
-        help(1)
+    parser = argparse.ArgumentParser(description="Ranks students' results", epilog='(C) 2017 -- Amr Ayman')
+    parser.add_argument('grade', choices=['J1', 'J2', 'J3', 'J4', 'J5', 'J6', 'M1', 'M2', 'M3', 'S1', 'S2'], help="Student's grade. e.g: J3, M2, ..")
+    parser.add_argument('outfile', help='Output file', type=argparse.FileType('w'))
+    parser.add_argument('benchnos', nargs='+', type=int, help='Student bench numbers')
+    parser.add_argument('-f', default='html', choices=['html', 'text', 'excel'], help='Output file format', dest='fileformat')
+    parser.add_argument( '--tops', default=10, type=int, help='How many tops ?')
+    options = parser.parse_args()
+    options.grade = {'J1': '1', 'J2': '2', 'J3': '3', 'J4': '4', 'J5': '5', 'J6': '6', 'M1': '7', 'M2': '8', 'M3': '9', 'S1': '10', 'S2': '11'}.get(options.grade)
+    return options
 
 def sort_results(results, keys):
     sorted_results = {}
@@ -52,18 +81,11 @@ def sort_results(results, keys):
         sorted_results[key] = sorted(results, key=lambda result: result.marks[key], reverse=True)
     return sorted_results
 
-def html_stuff(sort):
-    with open(options['out'], 'w') as f:
-        for key in sort.keys():
-            f.writelines('In %s:\n' % key)
-            for i, o in enumerate(sort[key]):
-                f.writelines('\t%02d. %s\t--->\t%f\n' % (i+1, o.name, o.marks[key]))
-
 if __name__ == '__main__':
-    parse_args()
+    options = parse_args()
     br = mechanize.Browser()
     br.open('http://new-sls.net/grades')
     print('Connecting ...')
-    for bench in options['benchs']:
-        results.append(Result(options['grade'], bench))
-    html_stuff(sort_results(results, results[0].marks.keys()))
+    for bench in options.benchnos:
+        results.append(Result(options.grade, bench))
+    write_table(sort_results(results, results[0].marks.keys()))
